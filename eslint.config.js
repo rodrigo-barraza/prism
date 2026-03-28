cf9884ee491a1b02d7e5b640046618e4ee6d8589
Rodrigo Barraza <hello@rod.dev>
2026-03-27 21:23:19 -0700

    chore: update ESLint configuration with globals package and refine project ignore rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,58 +1,48 @@
-import js from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   prettierConfig,
   {
     languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
-        process: 'readonly',
-        console: 'readonly',
-        Buffer: 'readonly',
-        URL: 'readonly',
-        fetch: 'readonly',
-        setInterval: 'readonly',
-        clearInterval: 'readonly',
-        setTimeout: 'readonly',
-        clearTimeout: 'readonly',
-        performance: 'readonly',
-        TextDecoder: 'readonly',
-        AbortController: 'readonly',
+        ...globals.node,
       },
     },
     rules: {
-      'no-unused-vars': [
-        'error',
+      "no-unused-vars": [
+        "error",
         {
-          argsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
         },
       ],
-      'no-console': 'off',
-      'prefer-const': 'error',
-      'no-var': 'error',
+      "no-console": "off",
+      "prefer-const": "error",
+      "no-var": "error",
     },
   },
   {
-    files: ['tests/**/*.js'],
+    files: ["tests/**/*.js"],
     languageOptions: {
       globals: {
-        describe: 'readonly',
-        it: 'readonly',
-        expect: 'readonly',
-        vi: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        vi: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
       },
     },
   },
   {
-    ignores: ['node_modules/'],
+    ignores: ["node_modules/"],
   },
 ];
